616db341d1e3a602aca68eacfe28c47c92aa931f
EstebanCaponio <estebancaponio@gmail.com>
2025-04-10 17:26:03 +0200

    feat: finish milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
-import { useState } from 'react';
-import films from './data/films';
+import { useEffect, useState } from 'react';
+import dataFilms from './data/films';
 
 function App() {
+  // con ...new set,nuovo array con solo i generi, set fa in modo che non ci siano doppioni
+  const genres = [...new Set(dataFilms.map(item => item.genre))];
+  // console.log(genres);
 
-  // con ...new set sto usando spread e creo un nuovo array con solo i generi, set fa in modo che non ci siano doppioni
-  const genders = [...new Set(films.map(item => item.genre))];
-  console.log(genders)
+  const [filmsList, setfilmsList] = useState(dataFilms);
+  const [filterGenre, setfilterGenre] = useState('');
+  let results = dataFilms;
+
+  useEffect(() => {
+    console.log('ho cambiato option')
+
+    if (filterGenre !== '') {
+      results = dataFilms.filter(film => film.genre.includes(filterGenre));
+    }
+
+    setfilmsList(results);
+  }, [filterGenre]);
 
   return (
     <>
       <div>
-
-        <select>
-          <option>---</option>
-          {genders.map((gender, index) => <option key={index}>{gender}</option>)}
+        <select value={filterGenre} onChange={event => { setfilterGenre(event.target.value) }}>
+          <option value="">---</option>
+          {genres.map((gender, index) => <option key={index}>{gender}</option>)}
         </select>
 
         <div>
-          {films.map(film =>
+          {filmsList.map(film =>
             <div key={film.id}>
               <h3>{film.title}</h3>
               <p>{film.genre}</p>
