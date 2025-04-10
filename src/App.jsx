9f5f3ede2332257ebe3752b0e884c9df5454c7c2
EstebanCaponio <estebancaponio@gmail.com>
2025-04-10 15:34:11 +0200

    feat: created select with array methods

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,27 @@ import films from './data/films';
 
 function App() {
 
+  // con ...new set sto usando spread e creo un nuovo array con solo i generi, set fa in modo che non ci siano doppioni
+  const genders = [...new Set(films.map(item => item.genre))];
+  console.log(genders)
+
   return (
     <>
       <div>
 
+        <select>
+          <option>---</option>
+          {genders.map((gender, index) => <option key={index}>{gender}</option>)}
+        </select>
+
+        <div>
+          {films.map(film =>
+            <div key={film.id}>
+              <h3>{film.title}</h3>
+              <p>{film.genre}</p>
+              <hr />
+            </div>)}
+        </div>
       </div>
     </>
   )
