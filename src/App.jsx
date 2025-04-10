5c33e443f9cce396e5de85abd8078c7c530b213f
EstebanCaponio <estebancaponio@gmail.com>
2025-04-10 22:04:08 +0200

    feat: bonus 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,48 @@ import dataFilms from './data/films';
 function App() {
   // con ...new set,nuovo array con solo i generi, set fa in modo che non ci siano doppioni
   const genres = [...new Set(dataFilms.map(item => item.genre))];
-  // console.log(genres);
 
   const [filmsList, setfilmsList] = useState(dataFilms);
   const [filterGenre, setfilterGenre] = useState('');
+  const [searchTitle, setSearchTitle] = useState('');
+
   let results = dataFilms;
 
   useEffect(() => {
-    console.log('ho cambiato option')
+
+    console.log('ho cambiato option') // debug
 
     if (filterGenre !== '') {
-      results = dataFilms.filter(film => film.genre.includes(filterGenre));
+      results = results.filter(film => film.genre.includes(filterGenre));
+    }
+
+    if (searchTitle !== '') {
+      results = results.filter(film =>
+        film.title.toLowerCase().includes(searchTitle.toLowerCase())
+      );
     }
 
     setfilmsList(results);
-  }, [filterGenre]);
+  }, [filterGenre, searchTitle]);
+
+  const searchInputChange = (event) => {
+    setSearchTitle(event.target.value);
+  };
 
   return (
     <>
       <div>
-        <select value={filterGenre} onChange={event => { setfilterGenre(event.target.value) }}>
-          <option value="">---</option>
-          {genres.map((gender, index) => <option key={index}>{gender}</option>)}
-        </select>
+        <div>
+          <select value={filterGenre} onChange={event => { setfilterGenre(event.target.value) }}>
+            <option value="">---</option>
+            {genres.map((gender, index) => <option key={index}>{gender}</option>)}
+          </select>
+
+          <input type="text"
+            placeholder='titolo'
+            value={searchTitle}
+            onChange={searchInputChange} />
+        </div>
 
         <div>
           {filmsList.map(film =>
